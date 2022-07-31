f821c830b0ad68a16d58a6e602226d750a0245f6
NotSlypher <86952522+NotSlypher@users.noreply.github.com>
2022-07-31 19:04:39 +0530

    Create optidb.sh

---
+++ OSL/optidb.sh
@@ -0,0 +1,138 @@
+#!/bin/bash	
+
+#Ayush Gupta               Roll no 33228
+#L - 10 
+
+RED="\e[31m"
+MAGNETA="\e[35m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
+CYAN="\e[36m"
+YELLOW="\e[33m"
+BLUE="\e[34m"
+
+createf(){
+	#echo "\n${YELLOW}***********************************************${ENDCOLOR}" 
+	echo -e -n "${GREEN}\nEnter file name : ${ENDCOLOR}"
+	read file_name
+
+	# Checking if file already exists
+	search=`ls | grep $file_name | wc -l` 
+	
+	if (( $search > 0 ))
+	then
+		fname=$file_name
+		echo -e "${GREEN}Selected file named: $fname ${ENDCOLOR}"
+	else
+		fname=$file_name
+		touch $fname.txt
+		echo -e "${GREEN}Created file named: $fname ${ENDCOLOR}"
+		 
+		echo -e "MODELNO|TYPE|MODELNAME"  >> $fname.txt
+
+	fi
+}
+
+createf
+
+while(true)
+	do
+		# print the menu
+		echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
+		echo -e "${YELLOW}|                  MENU                       |${ENDCOLOR}"
+		echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+		echo -e "${YELLOW}|      1) Create or Select Address book       |${ENDCOLOR}"
+		echo -e "${YELLOW}|      2) View Address book                   |${ENDCOLOR}"
+		echo -e "${YELLOW}|      3) Insert a Record                     |${ENDCOLOR}"
+		echo -e "${YELLOW}|      4) Delete a Record                     |${ENDCOLOR}"
+		echo -e "${YELLOW}|      5) Update a Record                     |${ENDCOLOR}"
+		echo -e "${YELLOW}|      6) Search a Record                     |${ENDCOLOR}"
+		echo -e "${YELLOW}|      7) Exit                                |${ENDCOLOR}"
+		echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+		echo -e -n "\nEnter your choice : "
+		read choice
+		
+		#switch case
+		case "$choice" in
+			1)
+				createf
+			;;
+			
+			2) 
+				echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
+				echo -e "\n${GREEN}Current Address Book is shown below \n${ENDCOLOR}"
+				cat $fname.txt | column -t -s "|" | sort
+				echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
+			;;
+				
+			3)
+				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+				echo -e "\nInserting a new Record" 
+				
+				echo -n "Enter Model no. : "
+				while(true)
+					do
+						read modno
+						if [ ${#modno} -gt 0 ]
+						then
+							
+							break
+						else
+							echo -e "${RED}Model no. cannot be empty${ENDCOLOR}"
+						fi 
+					done
+					
+				echo -n "Enter type : "
+				read type
+				
+				echo -n "Enter model name : "
+				read mname
+				
+				echo -e "\nEntering record \n$modno \t $type \t $mname"			
+				echo "$modno|$type|$mname" >> $fname.txt
+			;;
+			
+			4)
+				echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
+				echo -e -n "Enter the record to be deleted : "
+				read delrec
+				flag=`cat $fname.txt | grep $delrec | wc -l`
+	
+				if [ $flag -eq 1 ] #checking valid Record
+				then 
+					deleted_item=`cat $fname.txt | grep $delrec`
+					sed -i "/^$delrec/d" $fname.txt
+						# Deleting Record
+					
+					echo -e "${GREEN}Record Deleted Successfuly\n Record: $deleted_item ${ENDCOLOR}"
+				
+				else
+					echo -e "${RED}Entered Record doesn't exist!!!{ENDCOLOR}{ENDCOLOR}"
+				fi
+				echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
+			;;
+
+			5)
+
+			;;
+			
+			6)
+				echo -e "\n***********************************************" 
+				echo -e -n "Enter the record to be searched : "
+				read searchRec
+				grep $searchRec $fname.txt
+				echo -e "***********************************************" 
+			;;
+			
+			7)
+				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+				echo -e "                  Exiting                      "
+				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+				break
+			;;
+
+			*)
+				echo -e "${RED}Invalid Input!!!${ENDCOLOR}"
+			;;	
+		esac
+	done

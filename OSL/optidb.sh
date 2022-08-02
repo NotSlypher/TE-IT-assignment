18099277c284a38c4a7dced040f77bdceb90fa2b
Ayush Gupta <86952522+NotSlypher@users.noreply.github.com>
2022-08-02 14:44:50 +0530

    Update optidb.sh

--- OSL/optidb.sh
+++ OSL/optidb.sh
@@ -3,7 +3,7 @@
 #Ayush Gupta               Roll no 33228
 #L - 10 
 
-RED="\e[31m""
+RED="\e[31m"
 GREEN="\e[32m"
 ENDCOLOR="\e[0m"
 YELLOW="\e[33m"
@@ -28,6 +28,85 @@ createf(){
 		echo -e "MODELNO|TYPE|MODELNAME"  >> $fname.txt
 
 	fi
+}
+
+displayf(){
+	echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
+	echo -e "\n${GREEN}Current Address Book is shown below \n${ENDCOLOR}"
+	cat $fname.txt | column -t -s "|" | sort
+	echo -e "\n${YELLOW}***********************************************${ENDCOLOR}"
+}
+
+insertf(){
+		echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+		echo -e "\nInserting a new Record" 
+				
+		echo -n "Enter Model no. : "
+		while(true)
+			do
+				read modno
+				if [ ${#modno} -gt 0 ]
+				then
+							
+					break
+				else
+					echo -e "${RED}Model no. cannot be empty${ENDCOLOR}"
+				fi 
+			done
+					
+		echo -n "Enter type : "
+		read type
+				
+		echo -n "Enter model name : "
+		read mname
+				
+		echo -e "\nEntering record \n$modno \t $type \t $mname"			
+		echo "$modno|$type|$mname" >> $fname.txt
+}
+
+deletef(){
+	echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
+	echo -e -n "Enter the record to be deleted : "
+	read delrec
+	flag=`cat $fname.txt | grep $delrec | wc -l`
+	
+	if [ $flag -eq 1 ] #checking valid Record
+	then 
+		deleted_item=`cat $fname.txt | grep $delrec`
+		sed -i "/^$delrec/d" $fname.txt
+			# Deleting Record
+					
+		echo -e "${GREEN}Record Deleted Successfuly\n Record: $deleted_item ${ENDCOLOR}"
+				
+	else
+		echo -e "${RED}Entered Record doesn't exist!!!{ENDCOLOR}{ENDCOLOR}"
+	fi
+	echo -e "${YELLOW}\n*********************************************${ENDCOLOR}"
+}
+
+updatef(){
+	echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
+	echo -e -n "Enter the record to be Updated : "
+	read uprec
+	flag=`cat $fname.txt | grep $uprec | wc -l`
+	
+	if [ $flag -eq 1 ] #checking valid Record
+	then 
+}
+
+searchf(){
+	echo -e "\n***********************************************" 
+	echo -e -n "Enter the record to be searched : "
+	read searchRec
+	grep $searchRec $fname.txt
+	echo -e "***********************************************" 
+}
+
+exitf(){
+	echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+	echo -e "                  Exiting                      "
+	echo -e "${YELLOW}***********************************************${ENDCOLOR}"
+	break
 }
 
 createf
@@ -56,76 +135,27 @@ while(true)
 			;;
 			
 			2) 
-				echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
-				echo -e "\n${GREEN}Current Address Book is shown below \n${ENDCOLOR}"
-				cat $fname.txt | column -t -s "|" | sort
-				echo -e "\n${YELLOW}***********************************************${ENDCOLOR}" 
+				displayf 
 			;;
 				
 			3)
-				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
-				echo -e "\nInserting a new Record" 
-				
-				echo -n "Enter Model no. : "
-				while(true)
-					do
-						read modno
-						if [ ${#modno} -gt 0 ]
-						then
-							
-							break
-						else
-							echo -e "${RED}Model no. cannot be empty${ENDCOLOR}"
-						fi 
-					done
-					
-				echo -n "Enter type : "
-				read type
-				
-				echo -n "Enter model name : "
-				read mname
-				
-				echo -e "\nEntering record \n$modno \t $type \t $mname"			
-				echo "$modno|$type|$mname" >> $fname.txt
+				insertf
 			;;
 			
 			4)
-				echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
-				echo -e -n "Enter the record to be deleted : "
-				read delrec
-				flag=`cat $fname.txt | grep $delrec | wc -l`
-	
-				if [ $flag -eq 1 ] #checking valid Record
-				then 
-					deleted_item=`cat $fname.txt | grep $delrec`
-					sed -i "/^$delrec/d" $fname.txt
-						# Deleting Record
-					
-					echo -e "${GREEN}Record Deleted Successfuly\n Record: $deleted_item ${ENDCOLOR}"
-				
-				else
-					echo -e "${RED}Entered Record doesn't exist!!!{ENDCOLOR}{ENDCOLOR}"
-				fi
-				echo -e "${YELLOW}\n*********************************************${ENDCOLOR}" 
+				deletef 
 			;;
 
 			5)
-
+				updatef
 			;;
 			
 			6)
-				echo -e "\n***********************************************" 
-				echo -e -n "Enter the record to be searched : "
-				read searchRec
-				grep $searchRec $fname.txt
-				echo -e "***********************************************" 
+				searchf
 			;;
 			
 			7)
-				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
-				echo -e "                  Exiting                      "
-				echo -e "${YELLOW}***********************************************${ENDCOLOR}"
-				break
+				exitf
 			;;
 
 			*)
